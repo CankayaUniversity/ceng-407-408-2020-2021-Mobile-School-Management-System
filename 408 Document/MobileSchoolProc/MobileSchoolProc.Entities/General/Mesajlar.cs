578329d9509ca57d74cbf211e70bdec3ef5fd264
Oğuz GÜL <72518895+oguzgul06@users.noreply.github.com>
2021-03-31 22:34:08 +0300

    Add files via upload

---
+++ 408 Document/MobileSchoolProc/MobileSchoolProc.Entities/General/Mesajlar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobilOkulProc.Entities.General
+{
+    public class Mesajlar<T> where T:class, new()
+    {
+        public bool Durum { get; set; }
+        public string Mesaj { get; set; }
+        public int KayitID { get; set; }
+        public T Nesne { get; set; }
+        public List<T> Liste { get; set; }
+    }
+}
