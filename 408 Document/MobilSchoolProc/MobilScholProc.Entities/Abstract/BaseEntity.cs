68e415fc33289ad5430dfd592b015dfb13fe7b4a
Oğuz GÜL <72518895+oguzgul06@users.noreply.github.com>
2021-03-31 22:21:25 +0300

    Add files via upload

---
+++ 408 Document/MobilSchoolProc/MobilScholProc.Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MobilOkulProc.Entities.Abstract
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ObjectID { get; set; }
+        public bool Status { get; set; }
+    }
+}
