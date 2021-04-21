b39e21bf371598eed0a5d076414a7a7fd797c59a
Murat Şanlısavaş <Sanlisavas@hotmail.com>
2021-04-21 21:47:44 +0300

    instantiation of needs and functions. seperation of controllers

---
+++ MobilOkulProc.WebApp/Controllers/BranchController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MobilOkulProc.Entities.Concrete;
+using MobilOkulProc.Entities.General;
+using MobilOkulProc.WebApp.ViewModels;
+using X.PagedList;
+using MobilOkulProc.WebApp.Controllers;
+using static MobilOkulProc.WebApp.Controllers.HomeController;
+
+namespace MobilOkulProc.WebApp.Controllers
+{
+    public class BranchController : Controller
+    {
+
+
+        public IActionResult ListBranch(string Search, int? page, Mesajlar<BRANCH> mb)
+        {
+            BranchListViewModel<BRANCH> m = new BranchListViewModel<BRANCH>();
+            ViewBag.NameSurname = needs.NameSurname;
+            
+            m.Mesajlar = function.Get<BRANCH>(mb, "Branch/Branch_List");
+            if (Search != null)
+            {
+               m.Mesajlar.Liste =  m.Mesajlar.Liste.Where(m => m.BranchName.ToLower().Contains(Search)).ToList();
+            }
+            m.PagedList = m.Mesajlar.Liste.ToPagedList(page ?? 1, 25);
+            if (mb.Mesaj != "")
+            {
+                m.Mesajlar = mb;
+            }
+            return View(m);
+        } 
+        public IActionResult AddBranch()
+        {
+            ViewBag.NameSurname = needs.NameSurname;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddBranch(Mesajlar<BRANCH> m)
+        {
+            m = function.Add_Update<BRANCH>(m, "Branch/Branch_Insert");
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(m);
+        }
+        public IActionResult DeleteBranch(int id)
+        {
+            Mesajlar<BRANCH> m = new Mesajlar<BRANCH>();
+            m = function.Get<BRANCH>(m,"Branch/Branch_Select?BranchID="+id);
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(m);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteBranch(Mesajlar<BRANCH> mb)
+        {
+            mb = function.Get<BRANCH>(mb, "Branch/Branch_Delete?BranchID="+mb.Nesne.ObjectID);
+            ViewBag.NameSurname = needs.NameSurname;
+            if (mb.Mesaj == "Bilgiler silindi")
+            {
+                return RedirectToAction("ListBranch", "Branch", mb);
+            }
+            return View(mb);
+        }
+        public IActionResult DetailsBranch(int id)
+        {
+            Mesajlar<BRANCH> m = new Mesajlar<BRANCH>();
+            m = function.Get<BRANCH>(m, "Branch/Branch_Select?BranchID=" + id);
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(m);
+        }
+        public IActionResult EditBranch(int id)
+        {
+            Mesajlar<BRANCH> m = new Mesajlar<BRANCH>();
+            m = function.Get<BRANCH>(m, "Branch/Branch_Select?BranchID=" + id);
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(m);
+        }
+        [HttpPost]
+        public IActionResult EditBranch(Mesajlar<BRANCH> m)
+        {
+            m = function.Add_Update<BRANCH>(m, "Branch/Branch_Update");
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(m);
+        }
+        
+    }
+}
--- MobilOkulProc.WebApp/Controllers/HomeController.cs
+++ MobilOkulProc.WebApp/Controllers/HomeController.cs
@@ -17,12 +17,12 @@ namespace MobilOkulProc.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        string WebApiUrl = "";
-        private  static string  NameSurname = "";
+        public static Functions function = new Functions();
+        public static Needs needs = new Needs();
 
         public HomeController(IConfiguration cfg)
         {
-            WebApiUrl = cfg.GetValue<string>("WebApiUrl");
+            needs.WebApiUrl = cfg.GetValue<string>("WebApiUrl");
         }
 
 
@@ -30,210 +30,95 @@ namespace MobilOkulProc.WebApp.Controllers
         {
             if (user.NameSurname != null)
             {
-                NameSurname = user.NameSurname;
+                needs.NameSurname = user.NameSurname;
 
             }
-            ViewBag.NameSurname = NameSurname;
+            ViewBag.NameSurname = needs.NameSurname;
             return View();
+        }
+        public class Needs
+        {
+            public string WebApiUrl = "";
+            public string NameSurname = "";
         }
 
-        public IActionResult ListBranch(string Search, int? page, Mesajlar<BRANCH> mb)
+        public class Functions
         {
-            BranchListViewModel<BRANCH> m = new BranchListViewModel<BRANCH>();
-            ViewBag.NameSurname = NameSurname;
-            
-            m.Mesajlar = Get<BRANCH>(mb, "Branch/Branch_List");
-            if (Search != null)
+            public  Mesajlar<T> Add_Update<T>(Mesajlar<T> m, string ApiURL) where T : class, new()
             {
-               m.Mesajlar.Liste =  m.Mesajlar.Liste.Where(m => m.BranchName.ToLower().Contains(Search)).ToList();
-            }
-            m.PagedList = m.Mesajlar.Liste.ToPagedList(page ?? 1, 25);
-            if (mb.Mesaj != "")
-            {
-                m.Mesajlar = mb;
-            }
-            return View(m);
-        } 
-        public IActionResult AddBranch()
-        {
-            ViewBag.NameSurname = NameSurname;
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult AddBranch(Mesajlar<BRANCH> m)
-        {
-            m = Add_Update<BRANCH>(m, "Branch/Branch_Insert");
-            ViewBag.NameSurname = NameSurname;
-            return View(m);
-        }
-        public IActionResult DeleteBranch(int id)
-        {
-            Mesajlar<BRANCH> m = new Mesajlar<BRANCH>();
-            m = Get<BRANCH>(m,"Branch/Branch_Select?BranchID="+id);
-            ViewBag.NameSurname = NameSurname;
-            return View(m);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteBranch(Mesajlar<BRANCH> mb)
-        {
-            mb = Get<BRANCH>(mb, "Branch/Branch_Delete?BranchID="+mb.Nesne.ObjectID);
-            ViewBag.NameSurname = NameSurname;
-            if (mb.Mesaj == "Bilgiler silindi")
-            {
-                return RedirectToAction("ListBranch", "Home", mb);
-            }
-            return View(mb);
-        }
-        public IActionResult DetailsBranch()
-        {
-            ViewBag.NameSurname = NameSurname;
-            return View();
-        }
-        public IActionResult EditBranch()
-        {
-            ViewBag.NameSurname = NameSurname;
-            return View();
-        }
-        public Mesajlar<T> Delete_Select<T>(Mesajlar<T> m,string ApiURL) where T:class,new()
-        {
-
-            try
-            {
-                using (HttpClientHandler handler = new HttpClientHandler())
+                try
                 {
-                    using (HttpClient c = new HttpClient(handler))
+                    using (HttpClientHandler handler = new HttpClientHandler())
                     {
-                        string url = WebApiUrl + ApiURL;
-
-                        StringContent content = new StringContent(JsonConvert.SerializeObject(m.Nesne), System.Text.Encoding.UTF8, "application/json");
-
-                        using (var response = c.PostAsync(url, content))
+                        using (HttpClient c = new HttpClient(handler))
                         {
-                            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+                            string url = needs.WebApiUrl + ApiURL;
+
+                            StringContent content = new StringContent(JsonConvert.SerializeObject(m.Nesne), System.Text.Encoding.UTF8, "application/json");
+
+                            using (var response = c.PostAsync(url, content))
                             {
-                                var sonuc = response.Result.Content.ReadAsStringAsync();
-                                sonuc.Wait();
-
-                                var msg = JsonConvert.DeserializeObject<Mesajlar<T>>(sonuc.Result);
-
-                                if (msg.Mesaj == null)
+                                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
                                 {
-                                    m.Durum = false;
-                                    m.Mesaj = "Bir şeyer ters gitti.";
-                                    m.Status = "danger";
+                                    var sonuc = response.Result.Content.ReadAsStringAsync();
+                                    sonuc.Wait();
+
+                                    var msg = JsonConvert.DeserializeObject<Mesajlar<T>>(sonuc.Result);
+
+                                    m = msg;
+
                                 }
-                                else
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    m.Durum = false;
+                    m.Mesaj = ex.Message;
+                    m.Status = "danger";
+                }
+
+                return m;
+            }
+            public Mesajlar<T> Get<T>(Mesajlar<T> m, string ApiURL) where T : class, new()
+            {
+                try
+                {
+                    using (HttpClientHandler handler = new HttpClientHandler())
+                    {
+                        using (HttpClient c = new HttpClient(handler))
+                        {
+                            string url = needs.WebApiUrl + ApiURL;
+
+                            //StringContent content = new StringContent(JsonConvert.SerializeObject(m.Mesajlar.Nesne), System.Text.Encoding.UTF8, "application/json");
+
+                            using (var response = c.GetAsync(url))
+                            {
+                                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
                                 {
+                                    var sonuc = response.Result.Content.ReadAsStringAsync();
+                                    sonuc.Wait();
+
+                                    var msg = JsonConvert.DeserializeObject<Mesajlar<T>>(sonuc.Result);
+
                                     m = msg;
                                 }
                             }
                         }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                m.Durum = false;
-                m.Mesaj = ex.Message;
-                m.Status = "danger";
-            }
-
-            return m;
-        }
-        public Mesajlar<T> Add_Update<T>(Mesajlar<T> m, string ApiURL) where T:class, new()
-        {
-            try
-            {
-                using (HttpClientHandler handler = new HttpClientHandler())
+                catch (Exception ex)
                 {
-                    using (HttpClient c = new HttpClient(handler))
-                    {
-                        string url = WebApiUrl + ApiURL;
-
-                        StringContent content = new StringContent(JsonConvert.SerializeObject(m.Nesne), System.Text.Encoding.UTF8, "application/json");
-
-                        using (var response = c.PostAsync(url, content))
-                        {
-                            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
-                            {
-                                var sonuc = response.Result.Content.ReadAsStringAsync();
-                                sonuc.Wait();
-
-                                var msg = JsonConvert.DeserializeObject<Mesajlar<T>>(sonuc.Result);
-
-                                if (msg.Nesne != null)
-                                {
-                                    HttpContext.Session.SetObject("user", msg.Nesne);
-                                }
-                                else
-                                {
-                                    m.Durum = false;
-                                    m.Mesaj = "Bir şeyer ters gitti.";
-                                    m.Status = "danger";
-                                    return m;
-                                }
-                            }
-                        }
-                    }
+                    m.Durum = false;
+                    m.Mesaj = ex.Message;
+                    m.Status = "danger";
                 }
-            }
-            catch (Exception ex)
-            {
-                m.Durum = false;
-                m.Mesaj = ex.Message;
-                m.Status = "danger";
-            }
 
-            return m;
+                return m;
+            }
         }
-        public Mesajlar<T> Get<T>(Mesajlar<T> m, string ApiURL) where T : class, new()
-        {
-            try
-            {
-                using (HttpClientHandler handler = new HttpClientHandler())
-                {
-                    using (HttpClient c = new HttpClient(handler))
-                    {
-                        string url = WebApiUrl + ApiURL;
-
-                        //StringContent content = new StringContent(JsonConvert.SerializeObject(m.Mesajlar.Nesne), System.Text.Encoding.UTF8, "application/json");
-
-                        using (var response = c.GetAsync(url))
-                        {
-                            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
-                            {
-                                var sonuc = response.Result.Content.ReadAsStringAsync();
-                                sonuc.Wait();
-
-                                var msg = JsonConvert.DeserializeObject<Mesajlar<T>>(sonuc.Result);
-
-                                if (msg.Liste != null || msg.Nesne != null || msg != null)
-                                {
-                                    m = msg;
-                                }
-                                else
-                                {
-                                    m.Durum = false;
-                                    m.Mesaj = "Bir şeyler ters gitti.";
-                                    m.Status = "danger";
-                                    return m;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                m.Durum = false;
-                m.Mesaj = ex.Message;
-                m.Status = "danger";
-            }
-            
-            return m;
-        }
+        
 
 
 
