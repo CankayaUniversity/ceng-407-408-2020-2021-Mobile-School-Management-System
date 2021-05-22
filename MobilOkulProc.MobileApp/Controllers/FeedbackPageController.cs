3143dfdb64f5ed7324b1f9f93ed357cfe16879c4
oguz gül <ogzgl95@outlook.com>
2021-05-22 20:36:13 +0300

    Updated FeedbackPage

--- MobilOkulProc.MobileApp/Controllers/FeedbackPageController.cs
+++ MobilOkulProc.MobileApp/Controllers/FeedbackPageController.cs
@@ -1,6 +1,10 @@
 ﻿
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MobilOkulProc.Entities.Concrete;
+using MobilOkulProc.Entities.General;
+using MobilOkulProc.MobileApp.Models;
 using static MobilOkulProc.MobileApp.Controllers.HomePageController;
 
 namespace MobilOkulProc.MobileApp.Controllers
@@ -13,7 +17,33 @@ namespace MobilOkulProc.MobileApp.Controllers
             return View();
         }
 
-       
+
+        public IActionResult Add()
+        {
+
+            Mesajlar<USER> m = new Mesajlar<USER>();
+            m = function.Get<USER>(m, "User/User_List");
+            FeedbackPageModel<FEEDBACK> viewModel = new FeedbackPageModel<FEEDBACK>()
+            {
+                List = new SelectList(m.Liste, "ObjectID", "NameSurname"),
+                SelectedId = -1,
+            };
+
+            ViewBag.NameSurname = needs.NameSurname;
+            return View(viewModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(FeedbackPageModel<FEEDBACK> m)
+        {
+            m.Mesajlar.Nesne.UserID = m.SelectedId;
+            m.Mesajlar = function.Add_Update<FEEDBACK>(m.Mesajlar, "Feedback/Feedback_Insert");
+            ViewBag.NameSurname = needs.NameSurname;
+            return RedirectToAction("List", "Feedback", m.Mesajlar);
+        }
+
 
     }
        
