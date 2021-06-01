e4704390552426e9016568c2ad57a93464b0cd6f
msanlisavas <m.sanlisavas@savatel.com>
2021-06-01 15:55:40 +0300

    Syllabus added

---
+++ MobilOkulProc.WebApp/ViewModels/SyllabusViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MobilOkulProc.Entities.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace MobilOkulProc.WebApp.ViewModels
+{
+    public class SyllabusViewModel<T> where T : class, new()
+    {
+        public Mesajlar<T> Mesajlar { get; set; }
+        public IPagedList<T> PagedList { get; set; }
+        public SelectList LectureList { get; set; }
+        public int LectureId { get; set; }
+        public SelectList DaysList { get; set; }
+        public int DaysId { get; set; }
+    }
+}
