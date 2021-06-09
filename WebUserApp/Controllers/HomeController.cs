f2f6753d825dfbb5f04be0e7e6a129e499ef362d
msanlisavas <m.sanlisavas@savatel.com>
2021-06-09 16:46:48 +0300

    Layout and Welcome page finished.

--- WebUserApp/Controllers/HomeController.cs
+++ WebUserApp/Controllers/HomeController.cs
@@ -18,15 +18,12 @@ namespace WebUserApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Welcome()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- WebUserApp/Startup.cs
+++ WebUserApp/Startup.cs
@@ -1,29 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Serialization;
 
 namespace WebUserApp
 {
     public class Startup
     {
+        IConfiguration Configuration;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        private readonly string key = "Bu benim uzun string deðerim";
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+
+            services.AddMvc(x => x.EnableEndpointRouting = false)
+                .AddViewOptions(opt => opt.HtmlHelperOptions.ClientValidationEnabled = true)
+                .AddNewtonsoftJson(opt => opt.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddCors();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddRazorPages();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,25 +38,18 @@ namespace WebUserApp
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader());
+
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseSession();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(configureRoutes =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                configureRoutes.MapRoute(name: "default", template: "{controller=Home}/{action=Welcome}/{id?}");
             });
+
         }
     }
 }
