ec0eab1fdfec0f67fdf96b7b139d2af5522ae950
msanlisavas <m.sanlisavas@savatel.com>
2021-06-11 15:02:00 +0300

    Responsive Web Front-End working for Student.

---
+++ WebUserApp/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilOkulProc.WebUserApp.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Mailbox()
+        {
+            return View();
+        }
+        public IActionResult Compose()
+        {
+            return View();
+        }
+        public IActionResult Read()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUserApp/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilOkulProc.WebUserApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Announcements()
+        {
+            return View();
+        }
+        public IActionResult Teachers()
+        {
+            return View();
+        }
+        public IActionResult Notes()
+        {
+            return View();
+        }
+        public IActionResult Exams()
+        {
+            return View();
+        }
+        public IActionResult Lectures()
+        {
+            return View();
+        }
+
+    }
+}
